f0aeb57fd69aa8a805798816a50a3128063d68e0
llisnikowski <l.lisnikowski@gmail.com>
2024-03-23 22:09:03 +0100

    Add option

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,15 @@
 #include "app_environment.h"
 #include "import_qml_components_plugins.h"
 #include "import_qml_plugins.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
+#if HELLO
+    std::cout << "hello world" << std::endl;
+    return 0;
+#endif
+
     set_qt_environment();
 
     QGuiApplication app(argc, argv);
